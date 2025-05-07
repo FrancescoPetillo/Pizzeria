6e76d0dc5f6cbc8369e169bf20e33f4b5151e9cc
Francesco <165157246+FrancescoPetillo@users.noreply.github.com>
2025-05-07 20:10:40 +0200

    Primo commit del progetto

---
+++ pizclient/src/app/orderpizza/orderpizza.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
+import { OrderService } from '../order.service';
+//import { pizzas } from 'src/app/data/pizze.json';
+
+@Component({
+  selector: 'app-orderpizza',
+  templateUrl: './orderpizza.component.html',
+  styleUrls: ['./orderpizza.component.css']
+})
+export class OrderpizzaComponent implements OnInit  {
+
+  pizzas: any = [];
+  flags: boolean[] = [];
+
+  constructor(private orderservice: OrderService, private cartservice: CartService) {
+    
+  }
+
+
+  ngOnInit(): void {
+    this.orderservice.getPizzas().subscribe((data) => {
+      this.pizzas = data;
+    });
+    for (let i = 0; i < this.cartservice.cart.length; i++) {
+      this.flags[this.cartservice.cart[i].id] = true;
+    }
+  }
+
+  addToCart(pizza) {
+    this.cartservice.addToCart(pizza);
+    this.cartservice.getTotal();
+    this.flags[pizza.id] = !this.flags[pizza.id];
+  }
+
+  remove(pizza) {
+    this.cartservice.removeFromCart(pizza);
+    this.cartservice.getTotal();
+    this.flags[pizza.id] = !this.flags[pizza.id];
+  }
+
+}
+
